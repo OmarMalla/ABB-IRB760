01216a32874056e6fbee05bd28ea74add7138825
Omar Malla <92977092+OmarMalla@users.noreply.github.com>
2024-09-19 16:15:15 +0530

    Update IRB760.m

--- IRB760.m
+++ IRB760.m
@@ -4,9 +4,8 @@ IRB760 Simulation using the DH Apporach based on a simplified model.
 
 Created as part of:
 Malla, O. and Shanmugavel, M. (2024),
-"Simplified model to study the kinematics of manipulators
-with parallelogram linkages", Industrial Robot,
-Vol. ahead-of-print No. ahead-of-print.
+"Simplified model to study the kinematics of manipulators with parallelogram linkages",
+Industrial Robot, Vol. 51 No. 5, pp. 704-714.
 https://doi.org/10.1108/IR-01-2024-0046 
 
 The code requires the use of Peter Corke's Robotics Toolbox for Matlab:
